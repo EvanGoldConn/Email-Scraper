fee6c4a642f8b051f279949975b33cd2fc965929
EvanGoldConn <49243207+EvanGoldConn@users.noreply.github.com>
2020-12-29 16:11:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -138,7 +138,14 @@ RNRScraper()
 
 
 
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
 
 
+find_nth("foofoofoofoo", "foofoo", 2)
 
     
